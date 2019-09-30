6e313b01e50294f0e892be4f85ca49587ad0b155
ktatu <tatu.kaikkonen@helsinki.fi>
2019-09-30 17:06:53 +0300

    frontend lisättynä buildina

---
+++ build/precache-manifest.2f2e186d4a68715516a109957bf73f7e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa1383b89d94f24640477c0fc561fd22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cf798c52e45e8e8042e",
+    "url": "/static/js/2.d3c593ce.chunk.js"
+  },
+  {
+    "revision": "17ca439918443a510f0f",
+    "url": "/static/js/main.5510974f.chunk.js"
+  },
+  {
+    "revision": "bd15de2c81a3438b90f1",
+    "url": "/static/js/runtime~main.11bda596.js"
+  }
+]);
---
+++ build/precache-manifest.7ebe9dd2c67e36e037ab242de807a11a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d60957dc09254b9102b33d512982ac0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cf798c52e45e8e8042e",
+    "url": "/static/js/2.d3c593ce.chunk.js"
+  },
+  {
+    "revision": "882c1f70d7d69a4876db",
+    "url": "/static/js/main.12b967e6.chunk.js"
+  },
+  {
+    "revision": "bd15de2c81a3438b90f1",
+    "url": "/static/js/runtime~main.11bda596.js"
+  }
+]);
